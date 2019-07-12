3afeb2ccee24245de8ee8df2ba8e45ba343ff811
andysomboonsin <andy.somboonsin@esa.edu.au>
2019-07-12 21:43:11 +0000

    2 commit

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 import flask
-import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def www_outage():
-    message = flask.request.headers['Host']+" "+socket.gethostname()
+    message = flask.request.headers['Host']
     return render_template("index.html", message=message)
 
 if __name__ == '__main__':
